43677a79cf43bc08b107f57bd5157a519a2d5262
Lawrence N. Masullo <lmasullo@gmail.com>
2020-12-13 10:56:54 -0600

    Initial

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+  // Get current year
+  const d = new Date();
+  const currYear = d.getFullYear();
+</script>
+
+<style>
+</style>
+
+<footer>
+  <img src="/images/logo_small.png" alt="LPA Logo" />
+  <p>&copy 2011-{currYear} Lampasas Pilots Association</p>
+</footer>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,37 @@
+<script>
+  // Function to show and hide hamburger menu links
+  function fnMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+</script>
+
+<style>
+</style>
+
+<header>
+  <div>
+    <img src="/images/logo_small.png" alt="Masullo Counseling Logo" />
+    <span>Masullo Counseling</span>
+  </div>
+
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#services">Services</a>
+    <a href="#forms">Client Forms</a>
+    <a href="#insurance">Insurance</a>
+    <a href="#about">About</a>
+    <a href="#contact">Contact</a>
+  </nav>
+
+  <!-- Hamburger menu icon -->
+  <img
+    id="hamburger"
+    src="images/hamburger_black.png"
+    alt="Menu"
+    on:click={fnMenu} />
+</header>
